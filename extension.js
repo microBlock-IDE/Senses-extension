45c4c8a5791da45d82b2b3dea224b0e7ab431a98
Max <7087907+maxpromer@users.noreply.github.com>
2023-01-14 21:08:53 +0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -3,7 +3,7 @@
     description: "Industrial Internet of Things platform (IIoT)",
     author: "microBlock",
     category: "Communication",
-    version: "2.0.0",
+    version: "2.1.0",
     icon: "/static/icon.png", // Category icon
     color: "#3498DB", // Category color (recommend some blocks color)
     blocks: [ // Blocks in Category
